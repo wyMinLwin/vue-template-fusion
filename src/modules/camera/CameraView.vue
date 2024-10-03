6a4616691c448e47efa4c5b2cb9565929c6b3321
wyMinLwin <waiyanminlwin421@gmail.com>
2024-10-03 17:45:56 +0700

    :sparkles: Upload Doc

--- src/modules/camera/CameraView.vue
+++ src/modules/camera/CameraView.vue
@@ -5,17 +5,61 @@ import Dialog from '@/components/ui/dialog/Dialog.vue'
 import DialogContent from '@/components/ui/dialog/DialogContent.vue'
 import DialogTrigger from '@/components/ui/dialog/DialogTrigger.vue'
 import DialogClose from '@/components/ui/dialog/DialogClose.vue'
+import { useLoaderStore } from '@/stores/loaderStore'
+import { DialogHeader, DialogTitle } from '@/components/ui/dialog'
 
 let stream: MediaStream | null = null
 let mediaRecorder: MediaRecorder | null = null
+
+const { loadingOn, loadingOff } = useLoaderStore()
 const cameraStatus = ref(false)
 const videoElement = ref<HTMLVideoElement | null>(null)
 const canvasElement = ref<HTMLCanvasElement | null>(null)
+const uploadedType = ref<'img' | 'video'>('img')
+const imageBlob = ref<Blob | null>(null)
+const videoBlob = ref<Blob | null>(null)
+const uploadResponseURL = ref('')
 const imageData = ref('')
 const facingMode = ref<'user' | 'environment'>('environment')
 const isRecording = ref(false)
 const recordedChunks: Blob[] = []
 const recordedVideoUrl = ref('')
+
+const uploadFileReviewDialog = ref(false)
+
+async function uploadFileToAPI(blob: Blob): Promise<void> {
+    uploadedType.value = blob.type.includes('image') ? 'img' : 'video'
+    const formData = new FormData()
+    formData.append('file', blob)
+
+    try {
+        // startLoading();
+        loadingOn()
+        const response = (await fetch(
+            'https://crc-api-test.azurewebsites.net/api/v1/Authentication/UploadFile',
+            {
+                method: 'POST',
+                body: formData
+            }
+        )) as Response
+
+        if (response !== undefined && response.status !== 200) {
+            console.error('Failed to upload file, status code:', response.status)
+            // return null;
+        }
+
+        const result = await response.json()
+        uploadResponseURL.value = result?.file
+        uploadFileReviewDialog.value = true
+        // return result?.file;
+    } catch (error) {
+        console.error('Error uploading file:', error)
+        // return null;
+    } finally {
+        // stopLoading();
+        loadingOff()
+    }
+}
 
 const takePhoto = () => {
     const context = canvasElement.value?.getContext('2d')
@@ -33,10 +77,15 @@ const takePhoto = () => {
         )
 
         const data = canvasElement.value.toDataURL('image/png')
+        canvasElement.value.toBlob(async (blob) => {
+            if (blob) {
+                imageBlob.value = blob
+            }
+        })
+
         imageData.value = data
     }
 }
-
 
 const openCamera = async () => {
     try {
@@ -81,7 +130,8 @@ const startRecording = () => {
             }
         }
         mediaRecorder.onstop = () => {
-            const blob = new Blob(recordedChunks, { type: 'video/webm' })
+            const blob = new Blob(recordedChunks, { type: 'video/mp4' })
+            videoBlob.value = blob
             recordedVideoUrl.value = URL.createObjectURL(blob)
             recordedChunks.length = 0
         }
@@ -113,7 +163,9 @@ onMounted(() => {
                 <div class="space-x-4">
                     <Button @click="takePhoto">Take Photo</Button>
                     <Button v-if="!isRecording" @click="startRecording">Record Video</Button>
-                    <Button v-else variant="destructive" @click="stopRecording">Stop Recording</Button>
+                    <Button v-else variant="destructive" @click="stopRecording"
+                        >Stop Recording</Button
+                    >
 
                     <Button v-if="!cameraStatus" @click="openCamera">Open Camera</Button>
                     <Button v-else variant="destructive" @click="closeCamera">Close Camera</Button>
@@ -121,23 +173,48 @@ onMounted(() => {
             </div>
 
             <div class="flex flex-col items-center gap-3">
+                <h2 v-if="imageData">Captured Image</h2>
                 <canvas ref="canvasElement"></canvas>
 
                 <div v-if="imageData" class="space-x-4">
                     <a :href="imageData" download="Very Nice Image">
                         <Button> Download Image </Button>
                     </a>
+                    <Button @click="uploadFileToAPI(imageBlob!)">Upload Image</Button>
                 </div>
             </div>
+            <Dialog>
+                <DialogTrigger as-child>
+                    <Button v-if="recordedVideoUrl">Preview Video</Button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Recorded Video</DialogTitle>
+                    </DialogHeader>
+                    <video :src="recordedVideoUrl" controls class="w-full h-auto"></video>
+                    <DialogClose @click="uploadFileToAPI(videoBlob!)">Upload</DialogClose>
+                    <DialogClose>Close</DialogClose>
+                </DialogContent>
+            </Dialog>
         </section>
-
-        <Dialog>
-            <DialogTrigger as-child>
-                <Button v-if="recordedVideoUrl">Preview Video</Button>
-            </DialogTrigger>
+        <Dialog v-model:open="uploadFileReviewDialog">
             <DialogContent>
-                <video :src="recordedVideoUrl" controls class="w-full h-auto"></video>
-                <DialogClose>Close</DialogClose>
+                <h1 class="text-xl font-bold">Upload File Review</h1>
+                <div class="flex justify-end space-x-4">
+                    <img
+                        v-if="uploadedType === 'img'"
+                        :src="uploadResponseURL"
+                        alt="Uploaded Image"
+                        class="w-full h-auto"
+                    />
+                    <video
+                        v-else
+                        :src="uploadResponseURL"
+                        controls
+                        alt="Uploaded Image"
+                        class="w-full h-auto"
+                    ></video>
+                </div>
             </DialogContent>
         </Dialog>
     </div>
